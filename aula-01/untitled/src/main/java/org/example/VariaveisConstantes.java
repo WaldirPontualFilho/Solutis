b2912da2be09c3e25f1b7c9cb77e35d364a7834e
WaldirPontualFilho <waldir.pro@outlook.com>
2024-08-26 15:17:15 -0300

    Commit 01

---
+++ aula-01/untitled/src/main/java/org/example/VariaveisConstantes.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class VariaveisConstantes {
+    public static void main(String[] args) {
+        int qtdeIrmaos = 2;
+        String sexo = "F";
+        float minhaAltura = 1.65f;
+        String meuNome = "Rosi Teixeira";
+        final int MES_NASCIMENTO = 12;
+
+        System.out.println("Olá! Meu nome é " + meuNome);
+        System.out.println("Sexo : " + sexo);
+        System.out.println("Altura : " + minhaAltura);
+        System.out.println("Mes de Nascimento : " + MES_NASCIMENTO);
+        System.out.println("Quantidade de irmãos: " + qtdeIrmaos);
+    }
+}
